d1ba470519663ca0b9197c2e82d3e8f536db214d
Simran-0024 <simranthecoder24@gmail.com>
2024-10-27 04:41:58 +0530

    rectified all issues
    treated the issue for not correctly generating the qr code

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let qrbox= document.getElementById("qr-box");
 let qrimage= document.getElementById("qr-image");
-const input= document.getElementById("input-box").innerHTML;
+const input= document.getElementById("input-box");
 let gen = document.querySelector("#gen");
 
 
-function generateQR(){
-  
-    
-    qrimage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + 
-    input.innerText;
-
+gen.addEventListener("click", generateQR);
+function generateQR() {
+    const msg = input.value;
+    if (msg) {
+        qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + msg;
+    }
+    else {
+        alert("Please enter text or a URL to generate a QR code.");
+    }
 }
-gen.addEventListener("click", generateQR());
+
+
 
